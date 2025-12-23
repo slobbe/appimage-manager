2a74a3ee5669e5dc394120928713a2f0d884f2fd
S. Lobbe <slobbe@lobbe.cc>
2025-12-23 13:12:24 +0100

    support realtive paths when adding an appimage

--- cmd/aim/main.go
+++ cmd/aim/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"flag"
 
@@ -13,6 +14,7 @@ func main() {
 	// declare `add` flags
 	addCmd := flag.NewFlagSet("add", flag.ExitOnError)
 	addMove := addCmd.Bool("move", false, "move AppImage to directory instead of copy")
+	addAbsolute := addCmd.Bool("a", false, "AppImage's absolute path is given")
 
 	// declare `remove` flags
 	removeCmd := flag.NewFlagSet("rm", flag.ExitOnError)
@@ -31,8 +33,19 @@ func main() {
 	switch os.Args[1] {
 	case "add":
 		addCmd.Parse(os.Args[2:])
-		fmt.Println("add", addCmd.Args()[0], "|| move app?", *addMove)
-		command.Add(addCmd.Args()[0], *addMove)
+		fmt.Println("add", addCmd.Args()[0], "|| move app?", *addMove, "abs path?", *addAbsolute)
+
+		if *addAbsolute {
+			command.Add(addCmd.Args()[0], *addMove)
+		} else {
+			dir, err := os.Getwd()
+			if err != nil {
+				fmt.Println("Error:", err)
+			}
+			fmt.Println("Current directory:", dir)
+
+			command.Add(filepath.Join(dir, addCmd.Args()[0]), *addMove)
+		}
 	case "list":
 		command.List()
 	case "rm":
