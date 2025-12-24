9dd98534224bdb92d71829f5444b22b7c4e0a12c
S. Lobbe <slobbe@lobbe.cc>
2025-12-24 16:31:20 +0100

    remove install-release script

--- install.sh
+++ install.sh
@@ -1,1 +1,12 @@
+#!/bin/sh
 install -Dm755 bin/aim "$HOME/.local/bin/aim"
+
+#set -e
+#repo="slobbe/appimage-manager"
+#bin="aim"
+#inst="$HOME/.local/bin"
+#
+#curl -sL "https://github.com/$repo/releases/latest/download/${bin}-linux-amd64.tar.gz" \
+#| tar -xz -C "$inst" "$bin"
+#chmod +x "$inst/$bin"
+#echo "installed to $inst/$bin"
