d3051b1fcfb52d88dea9626a1e0f4b2c6523d1e1
S. Lobbe <slobbe@lobbe.cc>
2025-12-26 14:24:10 +0100

    install bin from github release

--- install.sh
+++ install.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
-install -Dm755 bin/aim "$HOME/.local/bin/aim"
+set -e
+repo="slobbe/appimage-manager"
+bin="aim"
+inst="$HOME/.local/bin"
 
-#set -e
-#repo="slobbe/appimage-manager"
-#bin="aim"
-#inst="$HOME/.local/bin"
-#
-#curl -sL "https://github.com/$repo/releases/latest/download/${bin}-linux-amd64.tar.gz" \
-#| tar -xz -C "$inst" "$bin"
-#chmod +x "$inst/$bin"
-#echo "installed to $inst/$bin"
+curl -sL "https://github.com/$repo/releases/latest/download/${bin}-linux-amd64" \
+-o "$inst/$bin" #| tar -xz -C "$inst" "$bin"
+chmod +x "$inst/$bin"
+echo "installed to $inst/$bin"
