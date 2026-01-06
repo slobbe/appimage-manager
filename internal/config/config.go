30d691cb9c4f61aeadf6985974f36017ad4b4d23
S. Lobbe <slobbe@lobbe.cc>
2026-01-06 12:05:56 +0100

    remove temporary direcotry `.temp`, using app specific tmep dirs

--- internal/config/config.go
+++ internal/config/config.go
@@ -22,13 +22,11 @@ func init() {
 	AimDir = filepath.Join(home, ".appimage-manager")
 	DesktopDir = filepath.Join(home, ".local/share/applications")
 
-	TempDir = filepath.Join(AimDir, ".tmp")
-
 	DbSrc = filepath.Join(AimDir, "apps.json")
 }
 
 func EnsureDirsExist() error {
-	dirs := []string{AimDir, TempDir, DesktopDir}
+	dirs := []string{AimDir, DesktopDir}
 	for _, dir := range dirs {
 		if err := os.MkdirAll(dir, 0755); err != nil {
 			return err
